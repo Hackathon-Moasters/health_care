f02a5e1bf455b79d238e9fd72d3f440eb13d7599
anivartak <adobehackathon28@gmail.com>
2025-07-10 22:52:00 +0530

    s

---
+++ dr-prescription-backend/server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Doctor Prescription API is running...');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
